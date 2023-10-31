466a9598a2539b96fcb4adf3747ebb03070ba235
tomislav.lucic5 <tomislav.lucic111@gmail.com>
2023-10-31 17:26:09 +0100

    Add new example

---
+++ RuapLV1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuapLV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Tomislava Lučića!");
+            Console.ReadLine();
+        }
+    }
+}
